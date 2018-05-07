2bed6ec63333b1b5c896cc6cd66226aac15ea353
nabilahmeida <nabilah.m.aditya@gmail.com>
2018-05-08 06:43:38 +0700

    add exercises-9.js week 3

---
+++ exercises-9.js
@@ -0,0 +1,21 @@
+function cariMean(angka){
+  var input = angka.length;
+  var  hasil = 0;
+  var x = 0;
+    for(var a = 0; a < input; a++){
+       x = x + angka[a];
+       
+         
+    }
+
+    hasil = x/input;
+    hasil = Math.round(hasil);
+    return hasil;
+    
+}
+console.log(cariMean([1, 2, 3, 4, 5])); // 3
+console.log(cariMean([3, 5, 7, 5, 3])); // 5
+console.log(cariMean([6, 5, 4, 7, 3])); // 5
+console.log(cariMean([1, 3, 3])); // 2
+console.log(cariMean([7, 7, 7, 7, 7])); // 7
+
